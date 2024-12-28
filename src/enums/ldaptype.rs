4a17c5be9b6aa990aa89b0131cc4243d045b4152
g0h4n <g0h4n_0@protonmail.com>
2024-12-28 08:40:27 +0100

    Adding InsuancePolicies and optimization

--- src/enums/ldaptype.rs
+++ src/enums/ldaptype.rs
@@ -19,6 +19,7 @@ pub enum Type {
     EnterpriseCA,
     AIACA,
     CertTemplate,
+    IssuancePolicie,
     Unknown
 }
 
@@ -29,106 +30,74 @@ impl fmt::Debug for Type {
 }
 
 /// Get object type, like ("user","group","computer","ou", "container", "gpo", "domain" "trust").
-pub fn get_type(result: SearchEntry) -> std::result::Result<Type, Type>
-{
+pub fn get_type(result: SearchEntry) -> std::result::Result<Type, Type> {
     let result_attrs: HashMap<String, Vec<String>> = result.attrs;
 
-    let contains = | values: &Vec<String>, to_find: &str | { 
-        values.iter().any( |elem| elem == to_find ) 
+    let contains = |values: &Vec<String>, to_find: &str| {
+        values.iter().any(|elem| elem == to_find)
     };
 
-    //trace!("{:?}",&result_attrs);
+    let object_class_vals = result_attrs.get("objectClass");
+    let flags_vals = result_attrs.get("flags");
 
-    // For all entries I checked if is an user,group,computer,ou,domain
-    for (key, vals) in &result_attrs 
-    {
-        // If key isn't objectClass, skip this key
-        if key != "objectClass" {
-            continue;
-        }
-
-        // Type is user
-        if contains(vals, "person") && contains(vals, "user") && !contains(vals, "computer") && !contains(vals, "group")
-        {
-            return Ok(Type::User)
-        }
-        // Type is user if is service-account
-        if contains(vals, "msDS-GroupManagedServiceAccount")
-        {
-            return Ok(Type::User)
-        }
-        // Type is group
-        if contains(vals, "group")
-        {
-            return Ok(Type::Group)
-        }
-        // Type is computer
-        if contains(vals, "computer")
-        {
-            return Ok(Type::Computer)
-        }
-        // Type is ou
-        if contains(vals, "organizationalUnit")
-        {
-            return Ok(Type::Ou)
-        }
-        // Type is domain
-        if contains(vals, "domain")
-        {
-            return Ok(Type::Domain)     
-        }
-        // Type is Gpo
-        if contains(vals, "groupPolicyContainer")
-        {
-            return Ok(Type::Gpo)
-        }
-        // Type is foreignSecurityPrincipal
-        if contains(vals, "top") && contains(vals, "foreignSecurityPrincipal")
-        {
-            return Ok(Type::ForeignSecurityPrincipal)
-        }
-        // Type is Container
-        if (contains(vals, "top") && contains(vals, "container")) && !contains(vals, "groupPolicyContainer")
-        {
-            return Ok(Type::Container)
-        }
-        // Type is Trust domain
-        if contains(vals, "trustedDomain")
-        {
-            return Ok(Type::Trust)
-        }
-        // Type is ADCS Certificate Authority
-        if contains(vals, "certificationAuthority")
-        && result.dn.contains(DirectoryPaths::ROOT_CA_LOCATION) 
-        {
-            return Ok(Type::RootCA)
-        }
-        // Type is ADCS Certificate Authority
-        if contains(vals, "pKIEnrollmentService")
-        && result.dn.contains(DirectoryPaths::ENTERPRISE_CA_LOCATION) 
-        {
-            return Ok(Type::EnterpriseCA)
-        }
-        // Type is ADCS Certificate Template
-        if contains(vals, "pKICertificateTemplate")
-        && result.dn.contains(DirectoryPaths::CERT_TEMPLATE_LOCATION) 
-        {
-            return Ok(Type::CertTemplate)
-        }
-        // Type is AIACA
-        if contains(vals, "certificationAuthority")
-        && result.dn.contains(DirectoryPaths::AIA_CA_LOCATION) 
-        {
-            return Ok(Type::AIACA)
-        }
-        // Type is NtAuthStore for NTAUTHCERTIFICATES
-        if contains(vals, "certificationAuthority")
-        && result.dn.contains(DirectoryPaths::NT_AUTH_STORE_LOCATION) 
-        {
-            return Ok(Type::NtAutStore)
+    if let Some(vals) = object_class_vals {
+        match () {
+            _ if contains(vals, "person") && contains(vals, "user") && !contains(vals, "computer") && !contains(vals, "group") => {
+                return Ok(Type::User);
+            }
+            _ if contains(vals, "msDS-GroupManagedServiceAccount") => {
+                return Ok(Type::User);
+            }
+            _ if contains(vals, "group") => {
+                return Ok(Type::Group);
+            }
+            _ if contains(vals, "computer") => {
+                return Ok(Type::Computer);
+            }
+            _ if contains(vals, "organizationalUnit") => {
+                return Ok(Type::Ou);
+            }
+            _ if contains(vals, "domain") => {
+                return Ok(Type::Domain);
+            }
+            _ if contains(vals, "groupPolicyContainer") => {
+                return Ok(Type::Gpo);
+            }
+            _ if contains(vals, "top") && contains(vals, "foreignSecurityPrincipal") => {
+                return Ok(Type::ForeignSecurityPrincipal);
+            }
+            _ if contains(vals, "top") && contains(vals, "container") && !contains(vals, "groupPolicyContainer") => {
+                return Ok(Type::Container);
+            }
+            _ if contains(vals, "trustedDomain") => {
+                return Ok(Type::Trust);
+            }
+            _ if contains(vals, "certificationAuthority") && result.dn.contains(DirectoryPaths::ROOT_CA_LOCATION) => {
+                return Ok(Type::RootCA);
+            }
+            _ if contains(vals, "pKIEnrollmentService") && result.dn.contains(DirectoryPaths::ENTERPRISE_CA_LOCATION) => {
+                return Ok(Type::EnterpriseCA);
+            }
+            _ if contains(vals, "pKICertificateTemplate") && result.dn.contains(DirectoryPaths::CERT_TEMPLATE_LOCATION) => {
+                return Ok(Type::CertTemplate);
+            }
+            _ if contains(vals, "certificationAuthority") && result.dn.contains(DirectoryPaths::AIA_CA_LOCATION) => {
+                return Ok(Type::AIACA);
+            }
+            _ if contains(vals, "certificationAuthority") && result.dn.contains(DirectoryPaths::NT_AUTH_STORE_LOCATION) => {
+                return Ok(Type::NtAutStore);
+            }
+            _ if contains(vals, "msPKI-Enterprise-Oid") && result.dn.contains(DirectoryPaths::ISSUANCE_LOCATION) => {
+                if let Some(flags) = flags_vals {
+                    if contains(flags, "2") {
+                        return Ok(Type::IssuancePolicie);
+                    }
+                }
+            }
+            _ => {}
         }
     }
-    return Err(Type::Unknown)
+    Err(Type::Unknown)
 }
 
 /// Ldap directory path.
@@ -142,4 +111,5 @@ impl DirectoryPaths {
     pub const NT_AUTH_STORE_LOCATION    : &'static str = "CN=NTAuthCertificates,CN=Public Key Services,CN=Services,CN=Configuration";
     pub const PKI_LOCATION              : &'static str = "CN=Public Key Services,CN=Services,CN=Configuration";
     pub const CONFIG_LOCATION           : &'static str = "CN=Configuration";
+    pub const ISSUANCE_LOCATION         : &'static str = "CN=OID,CN=Public Key Services,CN=Services,CN=Configuration";
 }
