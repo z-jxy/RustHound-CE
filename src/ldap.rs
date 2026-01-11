c8a42870329e5f1a20bcae737c49e2ee1cefda89
z-jxy <zjxy.00@gmail.com>
2026-01-11 17:27:24 -0500

    update ntlm hash method for upstream changes

--- src/ldap.rs
+++ src/ldap.rs
@@ -61,11 +61,11 @@ pub async fn ldap_search<S: Storage<LdapSearchEntry>>(
         let res = match ldap_args.s_auth {
             LdapAuth::Password(ref password) => ldap.simple_bind(&ldap_args.s_username, password).await?.success(),
             LdapAuth::Ntlm(ref ntlm_hash) => {
-                // TODO: refactor `sasl_ntlmv2_bind_with_hash` to handle username@domain
+                // TODO: refactor `sasl_ntlm_bind_with_hash_sspi` to handle username@domain
                 // or refactor `ldap_constructor`
                 let (username, domain) = ldap_args._s_email.split_once('@').unwrap_or((&ldap_args.s_username, "not set"));
 
-                ldap.sasl_ntlmv2_bind_with_hash(username, domain, ntlm_hash.as_bytes()).await?.success()
+                ldap.sasl_ntlm_bind_with_hash_sspi(username, domain, ntlm_hash.as_bytes()).await?.success()
             },
         };
         match res {
